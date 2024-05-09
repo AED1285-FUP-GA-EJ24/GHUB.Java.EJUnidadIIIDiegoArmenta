528165793a0bd5ded1781ae3be9ac560c9e64f58
Diego Armenta <158300849+diegoarmenta@users.noreply.github.com>
2024-05-09 15:01:57 +0000

    aa

--- Promedio.java/Promedio.java
+++ Promedio.java/Promedio.java
@@ -1,12 +1,25 @@
 package Promedio;
 import java.util.Scanner;
-public void promedio {
+
+import javax.swing.CellEditor;
+public class Promedio{
+   public void promedio() {
     Scanner teclado = new Scanner(System.in);
 
     float r;
     int n;
-    final int CENTINELA * -1;
-    int acum_calf=
-
+    final int CENTINELA = -1;
+    int acum_calf=0,cuenta=0;
+    System.out.println("introduzva la primera calificacion (-1 para terminar)");
+    n = tecladonextInt();
+    while ( (n!=CENTINELA)){
+        cuenta++;
+        acum_calf += n;
+        System.out.println("PRoporciona SIGUIENTE calificacion" +
+                            cuenta+" (-1 para terminar):");
+        n =teclado.nextInt();
+        
+    }
+   }
 
 }
